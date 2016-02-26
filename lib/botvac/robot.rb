19d634cfd3d7fd9bac5e6284508823b8b62eb845
alyc100 <alyc100@hotmail.com>
2016-02-26 23:25:57 +0000

    Fix removal of start_cleaning function

--- lib/botvac/robot.rb
+++ lib/botvac/robot.rb
@@ -12,6 +12,20 @@ module Botvac
       self.secret  = secret
     end
 
+    def start_cleaning
+      connection.post("messages", JSON.dump(
+        {
+          reqId: "1",
+          cmd: "startCleaning",
+          params: {
+            category: 2,
+            mode: 2,
+            modifier: 2
+          }
+        }
+      )).body
+    end
+    
     def pause_cleaning
       connection.post("messages", JSON.dump({ reqId: "1",cmd: "pauseCleaning" })).body
     end
