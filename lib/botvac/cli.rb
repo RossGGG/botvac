55eff9c94991766f4e2b0bcbb9a46e01ae9b4174
alyc100 <alyc100@hotmail.com>
2016-03-01 21:32:40 +0000

    Minor spelling fix

--- lib/botvac/cli.rb
+++ lib/botvac/cli.rb
@@ -11,7 +11,7 @@ module Botvac
     desc 'login', 'login into neato cloudservices'
     def login
       email    = ask("Email:").to_s
-      password = ask("Pasword:", echo: false).to_s
+      password = ask("Password:", echo: false).to_s
 
       self.token = connection.post('/sessions', { platform: "ios", email: email, token: SecureRandom.hex(32), password: password }).body['access_token']
     end
